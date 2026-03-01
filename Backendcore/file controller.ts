fec654e2b47516d2e3605aa335689aa92dce739c
Okoneffiong88 <136741297+Okoneffiong88@users.noreply.github.com>
2026-03-01 06:21:52 +0100

    Delete Backendcore/file controller.ts

--- Backendcore/file controller.ts
+++
@@ -1,6 +0,0 @@
-import { generateUploadUrl } from "../services/s3.service";
-
-export async function generateSignedUrl(req, res) {
-  const { url, key } = await generateUploadUrl(req.user.userId);
-  res.json({ url, key });
-}
